5049253909b42a58273288bd2d14855ef0da2b5b
Ulyanka2901 <144227029+Ulyanka2901@users.noreply.github.com>
2023-10-27 20:34:46 +0300

    Add files via upload

--- ЗАД 7.pas
+++ ЗАД 7.pas
@@ -1,18 +1,19 @@
-﻿program FunctionCalculation;
+﻿program zad7;
 var
-  x, y, z: real;
+  x, y, h, r, c: real;
 begin
   writeln('Введите начало отрезка:');
-  readln(x);
+  readln(r);
   writeln('Введите конец отрезка:');
-  readln(y);
+  readln(c);
   writeln('Введите шаг:');
- readln(z);
+  readln(h);
+  x := r;
   writeln('x     |     y');
   writeln('--------------');
   repeat
     y := 3 * x * x - power(2, x);
     writeln(x:0:2, '   |   ', y:0:2);
-    x := x + z;
-  until x > y;
+    x := x + h;
+  until x > c;
 end.
